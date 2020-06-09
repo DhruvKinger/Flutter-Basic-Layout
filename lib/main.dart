2ba3d48f71ad6cd7bcaa824f0b09a8e9d33e5509
DhruvKinger <cse1785@gmail.com>
2020-06-10 04:35:03 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main()=>runApp(app());
+void main() => runApp(app());
 
 class app extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar:AppBar(
-          title:Text("Trying with first Project")
-        )
-
+        appBar: AppBar(title: Text("Trying with first Project")),
+        body: Center(
+            child:
+                Text("Hello flutter layout", style: TextStyle(fontSize: 24.0))),
+        floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.lightbulb_outline),
+            onPressed: () {
+              print("Rang ?");
+            }),
+        persistentFooterButtons: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add_comment),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.add_alarm),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.add_location),
+            onPressed: () {},
+          ),
+        ],
       ),
     );
   }
